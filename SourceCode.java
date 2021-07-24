6dbd65acdbbd85a9c9655b4fd85891e7b58158b1
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 00:45:21 +0900

    add programmers template

--- SourceCode.java
+++ SourceCode.java
@@ -1,2 +1,15 @@
+class Solution {
+}
+
 public class SourceCode {
+    public static void main(String[] args) {
+        Object[] inputs = new Object[]{
+
+        };
+
+        for (Object input : inputs) {
+
+            System.out.println();
+        }
+    }
 }
