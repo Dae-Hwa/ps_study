4682febfd9395098850e58fec0c612f6393712d5
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 02:50:04 +0900

    프로그래머스 짝수와 횰수 완료

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# [문제명](/)
+# [프로그래머스 - 짝수와 홀수](https://programmers.co.kr/learn/courses/30/lessons/12937)
 
 ## 요구사항
 
+정수 num이 짝수일 경우 "Even", 홀수일 경우 "Odd" 반환
+
 ## 제한사항
+
+num은 int 범위의 정수
+
+0 은 짝수
 
 ## 기타
--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,23 @@
 class Solution {
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{3},
+                new Object[]{4},
+                new Object[]{0}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            String result = new Solution().solution((int) arguments[0]);
 
-
-            System.out.println();
+            System.out.println(result);
         }
     }
 }
