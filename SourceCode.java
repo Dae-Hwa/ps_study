97c6ed84dbfeb2740324a9255d373878fe49ac88
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 00:55:33 +0900

    프로그래머스 template에 arguments 추가

--- SourceCode.java
+++ SourceCode.java
@@ -4,10 +4,13 @@ class Solution {
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-
+                new Object[]{}
         };
 
         for (Object input : inputs) {
+            Object[] arguments = (Object[]) input;
+
+
 
             System.out.println();
         }
