60fb89b1e51127fad08b453e4b72665cdd1a6534
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 02:44:40 +0900

    프로그래머스 제일 작은 수 제거하기 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,36 @@
+import java.util.Arrays;
+
 class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return new int[]{-1};
+        }
+
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        return Arrays.stream(arr)
+                       .filter(value -> value != min)
+                       .toArray();
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{
+                        new int[]{4, 3, 2, 1}
+                },
+                new Object[]{
+                        new int[]{10}
+                }
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            int[] result = new Solution().solution((int[]) arguments[0]);
 
-
-            System.out.println();
+            System.out.println(Arrays.toString(result));
         }
     }
 }
