d332331c935322826029750c9c1909eb3a10c0e6
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 04:10:03 +0900

    프로그래머스 직사각형 별찍기 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,16 @@
+import java.util.Scanner;
+
 class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            System.out.println("*".repeat(a));
+        }
+    }
 }
 
 public class SourceCode {
-    public static void main(String[] args) {
-        Object[] inputs = new Object[]{
-                new Object[]{}
-        };
-
-        for (Object input : inputs) {
-            Object[] arguments = (Object[]) input;
-
-
-
-            System.out.println();
-        }
-    }
 }
