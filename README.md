f487e1b83ad384a6d79f42301eff32b595906eb7
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 03:52:24 +0900

    프로그래머스 행렬의 덧셈 완료

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# [문제명](/)
+# [행렬의 덧셈](https://programmers.co.kr/learn/courses/30/lessons/12950)
 
 ## 요구사항
 
+행렬 arr1과 arr2 덧셈
+
 ## 제한사항
+
+arr1, arr2의 행과 열의 길이는 500을 넘지 않는다.
 
 ## 기타
--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,51 @@
+import java.util.Arrays;
+
 class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = Arrays.copyOf(arr1, arr1.length);
+
+        for (int i = 0; i < answer.length; i++) {
+            int[] row = answer[i];
+            for (int j = 0; j < row.length; j++) {
+                answer[i][j] += arr2[i][j];
+            }
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{
+                        new int[][]{
+                                {1, 2},
+                                {2, 3}
+                        },
+                        new int[][]{
+                                {3, 4},
+                                {5, 6}
+                        }
+                },
+                new Object[]{
+                        new int[][]{
+                                {1},
+                                {2}
+                        },
+                        new int[][]{
+                                {3},
+                                {4}
+                        }
+                }
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            int[][] result = new Solution().solution((int[][]) arguments[0], (int[][]) arguments[1]);
 
-
-            System.out.println();
+            System.out.println(Arrays.deepToString(result));
         }
     }
 }
