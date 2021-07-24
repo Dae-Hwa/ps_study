6e9a3126b66c85c741ea55742d9793f79c22f3b1
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 03:13:45 +0900

    프로그래머스 콜라츠 추측 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,38 @@
 class Solution {
+    public int solution(int num) {
+        int answer = 0;
+
+        double result = num;
+
+        while (result != 1) {
+            answer++;
+            if (500 < answer) {
+                return -1;
+            }
+
+            if (result % 2 == 0) {
+                result /= 2;
+            } else {
+                result = result * 3 + 1;
+            }
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{6},
+                new Object[]{16},
+                new Object[]{626331}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
-
-
-            System.out.println();
+            System.out.println(new Solution().solution((int) arguments[0]));
         }
     }
 }
