6d407573e8e98f871c816250e28284504bc2fde5
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 03:29:16 +0900

    프로그래머스 핸드폰 번호 가리기 완료

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# [문제명](/)
+# [핸드폰 번호 가리기](https://programmers.co.kr/learn/courses/30/lessons/12948)
 
 ## 요구사항
 
+문자열 phone_number의 뒷 4자리를 제외한 나머지를 전부 *로 가린 문자열을 리턴
+
 ## 제한사항
+
+4 <= s <= 20
 
 ## 기타
--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,24 @@
 class Solution {
+    public String solution(String phone_number) {
+        int numberOfExposure = 4;
+        int indexOfStartingExposure = phone_number.length() - numberOfExposure;
+
+        return "*".repeat(indexOfStartingExposure) + phone_number.substring(indexOfStartingExposure);
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{"01033334444"},
+                new Object[]{"027778888"},
+                new Object[]{"4444"}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
-
-
-            System.out.println();
+            System.out.println(new Solution().solution((String) arguments[0]));
         }
     }
 }
