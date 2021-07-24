917d26294829e544f4ec8439eae94fd4c0ab82aa
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 04:03:12 +0900

    프로그래머스 x만큼 간격이 있는 n개의 숫자 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,32 @@
+import java.util.Arrays;
+
 class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] + x;
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{2, 5},
+                new Object[]{4, 3},
+                new Object[]{-4, 2}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            long[] result = new Solution().solution((int) arguments[0], (int) arguments[1]);
 
-
-            System.out.println();
+            System.out.println(Arrays.toString(result));
         }
     }
 }
