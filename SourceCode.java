5388464beb73ec55ac87bfa47548799d94f864ab
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-25 03:41:43 +0900

    프로그래머스 하샤드 수 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,30 @@
 class Solution {
+    public boolean solution(int x) {
+        int temp = x;
+        int sumOfCiphers = 0;
+
+        while (0 < temp) {
+            sumOfCiphers += temp % 10;
+            temp /= 10;
+        }
+
+        return x % sumOfCiphers == 0;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{10},
+                new Object[]{12},
+                new Object[]{11},
+                new Object[]{13}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
-
-
-            System.out.println();
+            System.out.println(new Solution().solution((int) arguments[0]));
         }
     }
 }
