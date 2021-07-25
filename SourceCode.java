660a7ffc08ca27a134ff8ba6049f892d6fbaf3aa
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-26 01:10:42 +0900

    프로그래머스 최댓값과 최솟값 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
 class Solution {
+    public String solution(String s) {
+        Deque<Integer> numbers = Arrays.stream(s.split(" "))
+                                        .map(Integer::parseInt)
+                                        .sorted()
+                                        .collect(Collectors.toCollection(ArrayDeque::new));
+
+        return numbers.peekFirst() + " " + numbers.peekLast();
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{"1 2 3 4"},
+                new Object[]{"-1 -2 -3 -4"},
+                new Object[]{"-1 -1"}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            String result = new Solution().solution((String) arguments[0]);
 
-
-            System.out.println();
+            System.out.println(result);
         }
     }
 }
