3685231483cac751eb2cdc3ccab9adc0fcbb1e2d
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-26 01:22:26 +0900

    프로그래머스 숫자의 표현 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,37 @@
 class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int lastStartingPoint = 1;
+        int cur = 0;
+        for (int i = 1; i < n; i++) {
+            cur += i;
+
+            while (n <= cur) {
+                if (n == cur) {
+                    answer++;
+                }
+
+                cur -= lastStartingPoint;
+                lastStartingPoint++;
+            }
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{15}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            int result = new Solution().solution((Integer) arguments[0]);
 
-
-            System.out.println();
+            System.out.println(result);
         }
     }
 }
