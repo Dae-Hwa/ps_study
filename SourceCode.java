ad32fb95dba579d2e889a1933796d530ed9b61fe
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-26 00:54:22 +0900

    프로그래머스 최솟값 만들기 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,39 @@
+import java.util.Arrays;
+
 class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - i - 1];
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{
+                        new int[]{1, 4, 2},
+                        new int[]{5, 4, 4}
+                },
+                new Object[]{
+                        new int[]{1, 2},
+                        new int[]{3, 4}
+                }
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            int result = new Solution().solution((int[]) arguments[0], (int[]) arguments[1]);
 
-
-            System.out.println();
+            System.out.println(result);
         }
     }
 }
