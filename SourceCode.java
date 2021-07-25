0d2fc0346b7f1ddf4a6a6f436307fcadab3b29be
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-26 02:31:15 +0900

    프로그래머스 다음 큰 숫자 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,28 @@
 class Solution {
+    public int solution(int n) {
+        int answer = n + 1;
+
+        while (Integer.bitCount(n) != Integer.bitCount(answer)) {
+            answer++;
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{78},
+                new Object[]{15}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            int result = new Solution().solution((Integer) arguments[0]);
 
-
-            System.out.println();
+            System.out.println(result);
         }
     }
 }
