b57672cfe2892a9d965acd30194c92cc98601573
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-26 04:57:57 +0900

    프로그래머스 파일명 정렬 완료

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
-# [문제명](/)
+# [파일명 정렬](https://programmers.co.kr/learn/courses/30/lessons/17686)
 
 ## 요구사항
 
+HEAD-NUMBER-TAIL 과 같은 형식의 파일을 HEAD이후 NUMBMER 순으로 정렬
+
+파일은 files 배열로 주어진다.
+
 ## 제한사항
 
+files <= 1000
+
+파일명 <= 100
+
+파일명은 아래의 문자만 가능
+
+- 영어 대소문자
+- 숫자
+- 공백
+- 마침표
+- 하이픈(-)
+
+HEAD는 숫자가 아닌 문자. 한 글자 이상, 대소문자 구분하지 않음
+
+NUMBER는 최대 다섯 글자 사이의 연속된 숫자. 0부터 시작할 수 있다.
+
+TAIL은 나머지 부분
+
 ## 기타
+
+Arrays.sort는 퀵소트, Collections.sort는 머지소트라고 한다.
--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,96 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
+    public String[] solution(String[] files) {
+        return Arrays.stream(files)
+                       .map(File::from)
+                       .sorted((o1, o2) -> {
+                                   int comparingResult = Comparator.<String>naturalOrder().compare(o1.head(), o2.head());
+
+                                   if (comparingResult == 0) {
+                                       comparingResult = Comparator.<Integer>naturalOrder().compare(o1.number(), o2.number());
+                                   }
+
+                                   return comparingResult;
+                               }
+                       ).map(File::name)
+                       .toArray(value -> new String[value]);
+    }
+}
+
+class File {
+    private String name;
+    private String head;
+    private int number;
+
+    private File(String name, String head, int number) {
+        this.name = name;
+        this.head = head;
+        this.number = number;
+    }
+
+    public static File from(String name) {
+        StringBuilder head = new StringBuilder();
+        StringBuilder number = new StringBuilder();
+
+        for (char each : name.toCharArray()) {
+            if (isNumber(each)) {
+                number.append(each);
+            } else if (number.length() == 0 && !isNumber(each)) {
+                head.append(each);
+            } else if (head.length() != 0 && !isNumber(each)) {
+                break;
+            }
+        }
+
+        if(number.length() == 0) {
+            number.append(0);
+        }
+
+        return new File(name, head.toString().toLowerCase(), Integer.valueOf(number.toString()));
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String head() {
+        return head;
+    }
+
+    public int number() {
+        return number;
+    }
+
+    private static boolean isNumber(char c) {
+        return '0' <= c && c <= '9';
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{new String[]{
+                        "img12.png", "img10.png", "img02.png", "Img02.png", "img1.png", "IMG01.GIF", "img2.JPG"
+                }},
+                new Object[]{new String[]{
+                        "F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"
+                }},
+                new Object[]{new String[]{
+                        "F 5 Freedom Fighter", "F 014", "B 50 Superfortress", "A 10 Thunderbolt II", "F 14 Tomcat, f 14"
+                }},
+                new Object[]{new String[]{
+                        "a"
+                }}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            String[] result = new Solution().solution((String[]) arguments[0]);
 
-
-            System.out.println();
+            System.out.println(Arrays.toString(result));
         }
     }
 }
