003262c78bb69813896912851b25ef4407144706
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-26 04:02:13 +0900

    프로그래머스 올바른 괄호 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,36 @@
 class Solution {
+    boolean solution(String s) {
+        int cnt = 0;
+        for (char each : s.toCharArray()) {
+            if (cnt < 0) {
+                return false;
+            }
+
+            if (each == '(') {
+                cnt++;
+                continue;
+            }
+
+            cnt--;
+        }
+
+        return cnt == 0;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{"()()"},
+                new Object[]{"(())()"},
+                new Object[]{")()("},
+                new Object[]{"(()("}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
-
-
-            System.out.println();
+            System.out.println(new Solution().solution((String) arguments[0]));
         }
     }
 }
