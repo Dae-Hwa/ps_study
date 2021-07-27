59115178f51b009e4b8f091398dc1cfe6819a4b1
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-28 03:52:18 +0900

    프로그래머스 스킬트리 완료

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# [문제명](/)
+# [스킬트리](https://programmers.co.kr/learn/courses/30/lessons/49993)
 
 ## 요구사항
 
+스킬 트리 순서 skill이 주어진다.
+skill에 들어있는 순서대로 스킬을 배울 수 있다.
+
+skill에 들어있는 스킬 외의 스킬은 언제 배우든 상관 없다.
+
+스킬 트리 목록에서 가능한 경우의 수를 리턴
+
 ## 제한사항
+
+모든 문자열은 알파벳 대문자
+
+1 <= skill.length <= 26
+
+    스킬은 중복되지 않는다.
+
+1 <= skill_trees.length <= 20
+
+2 <= skill_trees(*).length <= 26
+
+    스킬은 중복되지 않는다.
 
 ## 기타
--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,63 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+
+        Set<Character> skillsCanNotLearning = new HashSet<>();
+
+        for (int i = 1; i < skill.length(); i++) {
+            skillsCanNotLearning.add(skill.charAt(i));
+        }
+
+        for (String skillTree : skill_trees) {
+            int skillIndex = 0;
+
+            Set<Character> currentSkillsCanNotLearning = new HashSet<>(skillsCanNotLearning);
+
+            boolean isFailed = false;
+
+            for (char c : skillTree.toCharArray()) {
+                if (currentSkillsCanNotLearning.contains(c)) {
+                    isFailed = true;
+                    break;
+                }
+
+                if (c == skill.charAt(skillIndex)) {
+                    skillIndex++;
+
+                    if (skillIndex == skill.length()) {
+                        break;
+                    } else {
+                        currentSkillsCanNotLearning.remove(skill.charAt(skillIndex));
+                    }
+                }
+
+            }
+
+            if (!isFailed) answer++;
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{
+                        "CBD",
+                        new String[]{"BACDE", "CBADF", "AECB", "BDA"}
+                }
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            int result = new Solution().solution((String) arguments[0], (String[]) arguments[1]);
 
-
-            System.out.println();
+            System.out.println(result);
         }
     }
 }
