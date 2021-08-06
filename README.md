304b2e24a98a4d52bc75ef3141f0d7a53237d093
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-07 00:16:23 +0900

    프로그래머스 약수의 개수와 덧셈 완료

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# [문제명](/)
+# [약수의 개수와 덧셈](https://programmers.co.kr/learn/courses/30/lessons/77884)
 
 ## 요구사항
 
+주어지는 left와 right 사이의 수 중에서 약수의 개수가 짝수면 더하고 홀수면 뺀 결과를 반환
+
 ## 제한사항
+
+1 <= left <= right <= 1000
 
 ## 기타
--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,41 @@
 class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int currentNumber = left; currentNumber <= right; currentNumber++) {
+            int submultipleCount = 0;
+            for (int candidate = 1; candidate <= currentNumber / candidate; candidate++) {
+                if (currentNumber % candidate == 0) {
+                    if (candidate == currentNumber / candidate) {
+                        submultipleCount++;
+                    } else {
+                        submultipleCount += 2;
+                    }
+                }
+            }
+
+            if (submultipleCount % 2 == 0) {
+                answer += currentNumber;
+            } else {
+                answer -= currentNumber;
+            }
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{13, 17},
+                new Object[]{24, 27}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
-
-
-            System.out.println();
+            System.out.println(new Solution().solution((int) arguments[0], (int) arguments[1]));
         }
     }
 }
