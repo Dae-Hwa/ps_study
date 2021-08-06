9eb33c4bfb295bfc800a9f0a86c05fe1e4a1bb1a
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-07 01:52:32 +0900

    백준 템플릿

--- SourceCode.java
+++ SourceCode.java
@@ -1,2 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
+
 public class SourceCode {
+    public static void main(String[] args) {
+        Object[] inputs = new Object[]{
+                new Object[]{}
+        };
+
+        for (Object input : inputs) {
+            Object[] arguments = (Object[]) input;
+
+            System.out.println();
+        }
+    }
 }
