d2bb0e3f02f4d514eac078cbf116c6a45c526998
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-11 23:12:01 +0900

    약수의 합 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,18 +1,29 @@
 class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n / i; i++) {
+            if (n % i == 0) {
+                answer += i;
+                if (i != n / i) answer += n / i;
+            }
+        }
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{12},
+                new Object[]{5}
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
-
-
-            System.out.println();
+            System.out.println(new Solution().solution((int) arguments[0]));
         }
     }
 }
