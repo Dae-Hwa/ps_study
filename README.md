5a35d321345c8db5968b865aa51e7bb2e28815eb
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-18 02:56:04 +0900

    슬라이딩 윈도우 실패 - 시간초과

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-# [문제명](/)
+# [Sliding Window Maximum](https://leetcode.com/problems/sliding-window-maximum/)
 
 ## 요구사항
 
+integer배열 nums가 있고, 사이즈가 k인 슬라이딩 윈도우가 있다. 이는 배열의 가장 왼쪽에서 오른쪽으로 움직인다.
+
+윈도우에서는 k개의 숫자만 볼 수 있다. 슬라이동은 한 번에 오른쪽으로 한 칸씩 움직인다.
+
+슬라이딩 윈도우의 max를 반환
+
 ## 제한사항
 
+1 <= nums.length <= 10^5
+
+-10^4 <= nums[i] <= 10^4
+
+1 <= k <= nums.length
+
 ## 기타
+
+그냥 배열만으로는 풀리지 않는다.
+
+중간에 홀딩할 무언가가 필요하다. 보통 deque를 많이 쓰는 듯 하다.
--- SourceCode.java
+++ SourceCode.java
@@ -1,17 +1,62 @@
+import java.util.Arrays;
+
 class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] answer = new int[nums.length - k + 1];
+        int maxIndex = -1;
+
+        int left = 0;
+        int right = k;
+
+        for (int i = k - 1; i < nums.length; i++, left++, right++) {
+            if (maxIndex < left) {
+                maxIndex = left;
+                for (int j = left; j < right; j++) {
+                    if (nums[maxIndex] < nums[j]) maxIndex = j;
+                }
+            }
+
+            if (nums[maxIndex] <= nums[i]) maxIndex = i;
+
+            answer[left] = nums[maxIndex];
+        }
+
+
+        return answer;
+    }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{
+                        new int[]{1, 3, -1, -3, 5, 3, 6, 7},
+                        3
+                },
+                new Object[]{
+                        new int[]{1},
+                        1
+                },
+                new Object[]{
+                        new int[]{1, -1},
+                        1
+                },
+//                new Object[]{
+//                        new int[]{9, 11},
+//                        2
+//                },
+//                new Object[]{
+//                        new int[]{4, -2},
+//                        2
+//                }
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
+            int[] result = new Solution().maxSlidingWindow((int[]) arguments[0], (int) arguments[1]);
 
-            System.out.println();
+            System.out.println(Arrays.toString(result));
         }
     }
 }
