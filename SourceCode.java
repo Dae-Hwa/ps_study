118be7feb328b05ebb40ada646524e3d9a5403f2
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-20 03:07:33 +0900

    백준 A와 B 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,23 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        String T = br.readLine();
+
+        System.out.println(solution(S, T));
+    }
+
+    public static int solution(String S, String T) {
+
+        Deque<Character> t = T.chars()
+                              .mapToObj(value -> (char) value)
+                              .collect(Collectors.toCollection(LinkedList::new));
+
+        while (S.length() < t.size()) {
+            if (t.peekLast() == 'B') {
+                t.pollLast();
+                Collections.reverse((List<Character>) t);
+            } else {
+                t.pollLast();
+            }
+        }
+
+        String result = t.stream()
+                         .map(character -> Character.toString(character))
+                         .collect(Collectors.joining());
+
+        return S.equals(result) ? 1 : 0;
     }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
-                new Object[]{}
+                new Object[]{
+                        "B",
+                        "ABBA"
+                },
+                new Object[]{
+                        "B",
+                        "ABBB"
+                },
+                new Object[]{
+                        "AB",
+                        "ABB"
+                },
+                new Object[]{
+                        "BA",
+                        "ABB"
+                }
         };
 
         for (Object input : inputs) {
             Object[] arguments = (Object[]) input;
 
-            System.out.println();
+            System.out.println(Main.solution((String) arguments[0], (String) arguments[1]));
         }
     }
 }
