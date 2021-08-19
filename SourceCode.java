7f904b5ed0043eedbcbaf763fc3bfebb7c568cbe
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-20 00:52:39 +0900

    leetcode sliding window 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,34 +1,69 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k == 1) return nums;
+
         int[] answer = new int[nums.length - k + 1];
-        int maxIndex = -1;
 
-        int left = 0;
-        int right = k;
+        Queue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(Node::value).reversed());
 
-        for (int i = k - 1; i < nums.length; i++, left++, right++) {
-            if (maxIndex < left) {
-                maxIndex = left;
-                for (int j = left; j < right; j++) {
-                    if (nums[maxIndex] < nums[j]) maxIndex = j;
-                }
-            }
-
-            if (nums[maxIndex] <= nums[i]) maxIndex = i;
-
-            answer[left] = nums[maxIndex];
+        for (int i = 0; i < k; i++) {
+            pq.add(new Node(i, nums[i]));
         }
 
+        /* nums.length - k + 1 회 만큼 돌아야 한다.
+         * k ~ nums.length + 1만큼 돌아야 하는데, 이러면 i가 헷갈리므로 k-1 ~ nums.length 만큼 반복
+         */
+        for (int i = k - 1; i < nums.length; i++) {
+            while (!pq.isEmpty() && pq.peek().isIndexSmallerThan(i - k + 1)) {
+                pq.poll();
+            }
+
+            pq.add(new Node(i, nums[i]));
+
+            answer[i - k + 1] = pq.peek().value();
+        }
 
         return answer;
+    }
+}
+
+class Node implements Comparable<Node> {
+    private int index;
+    private int value;
+
+    public Node(int index, int value) {
+        this.index = index;
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public boolean isIndexSmallerThan(int target) {
+        return index < target;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return Comparator.comparingInt(Node::value)
+                         .reversed()
+                         .compare(this, o);
     }
 }
 
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
+                new Object[]{
+                        new int[]{1, -1},
+                        2
+                },
                 new Object[]{
                         new int[]{1, 3, -1, -3, 5, 3, 6, 7},
                         3
@@ -41,14 +76,22 @@ public class SourceCode {
                         new int[]{1, -1},
                         1
                 },
-//                new Object[]{
-//                        new int[]{9, 11},
-//                        2
-//                },
-//                new Object[]{
-//                        new int[]{4, -2},
-//                        2
-//                }
+                new Object[]{
+                        new int[]{1, 3, 1, 2, 0, 5},
+                        3
+                },
+                new Object[]{
+                        new int[]{9, 10, 9, -7, -4, -8, 2, -6},
+                        5
+                },
+                new Object[]{
+                        new int[]{9, 11},
+                        2
+                },
+                new Object[]{
+                        new int[]{4, -2},
+                        2
+                }
         };
 
         for (Object input : inputs) {
