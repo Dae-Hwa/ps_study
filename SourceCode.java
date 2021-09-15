dea262a6d28589a6403790df291b110bd5a4c076
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-09-15 10:05:12 +0900

    template 업데이트

--- SourceCode.java
+++ SourceCode.java
@@ -1,2 +1,14 @@
 public class SourceCode {
+    public static void main(String[] args) {
+        Object[] inputs = new Object[]{
+                new Object[]{}
+        };
+
+        for (Object input : inputs) {
+            Object[] arguments = (Object[]) input;
+
+
+            System.out.println();
+        }
+    }
 }
