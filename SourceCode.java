59056b6c209e2dc78623ed43c5dace5bab518162
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-10-12 15:42:07 +0900

    프로그래머스 h-index 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,4 +1,22 @@
+import java.util.*;
+
 class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+
+        int max = 0;
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = i + 1;
+            int reverse = citations.length - i - 1;
+
+            if (h <= citations[reverse]) {
+                max = Math.max(max, h);
+            }
+        }
+
+        return max;
+    }
 }
 
 public class SourceCode {
