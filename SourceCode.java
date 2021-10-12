bc3412d559e1643c788c81e9d3eca28b2f2c0e51
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-10-12 17:19:47 +0900

    백준 행복유치원 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,10 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input1 = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input1[0]);
+        int K = Integer.parseInt(input1[1]);
+
+        int[] children = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+
+        int[] costs = new int[N - 1];
+
+        for (int i = 0; i < children.length - 1; i++) {
+            costs[i] = children[i + 1] - children[i];
+        }
+
+        Arrays.sort(costs);
+
+        int answer = 0;
+
+        for (int i = 0; i < N - K; i++) {
+            answer += costs[i];
+        }
+
+        System.out.println(answer);
     }
 }
 
