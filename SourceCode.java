467f831050f03f969e2d4d3cbc55902f8eb8256d
Dae-Hwa <daehwa_jeong@outlook.com>
2021-11-02 11:21:21 +0900

    프로그래머스 튜플 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Solution {
+    public int[] solution(String s) {
+
+        List<List<Integer>> split = Arrays.stream(s.split("},\\{"))
+                                          .map(str -> str.replaceAll("}", "")
+                                                         .replaceAll("\\{", "")
+                                          ).sorted(Comparator.comparing(o -> o.length()))
+                                          .map(str -> Arrays.stream(str.split(","))
+                                                            .map(Integer::valueOf)
+                                                            .collect(Collectors.toList())
+                                          ).collect(Collectors.toList());
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < split.size(); i++) {
+            List<Integer> cur = split.get(i);
+            cur.removeAll(result);
+            result.addAll(cur);
+        }
+
+        return result.stream().mapToInt(Integer::valueOf).toArray();
+    }
 }
 
 public class SourceCode {
