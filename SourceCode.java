3bddbe00da9a7b6e3bab51f3e48cac49244a3676
Dae-Hwa <daehwa_jeong@outlook.com>
2021-11-02 11:33:31 +0900

    백준 사토르 마방진 완료

--- SourceCode.java
+++ SourceCode.java
@@ -5,6 +5,32 @@ import java.io.InputStreamReader;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] set = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            set[i] = br.readLine();
+        }
+
+        String answer = "YES";
+
+        for (int i = 0; i < N; i++) {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < N; j++) {
+                sb.append(set[j].charAt(i));
+            }
+
+            if (!set[i].equals(sb.toString())) {
+                answer = "NO";
+                break;
+            }
+        }
+
+        System.out.println(answer);
     }
 }
 
