8ee85f62c774fe9a6124f94a8cffd54b87ebe44f
Dae-Hwa <daehwa_jeong@outlook.com>
2021-11-28 10:20:00 +0900

    프로그래머스 조이스틱 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,4 +1,44 @@
+import java.util.*;
+
 class Solution {
+    public int solution(String name) {
+        int counter = -1;
+        for (char c : name.toCharArray()) {
+            counter++;
+            counter += Math.min(c - 'A', 'Z' - c + 1);
+        }
+
+        boolean flag = true;
+        int toLeft = 0;
+        for (int i = 1; i < name.length(); i++) {
+            if (flag && name.charAt(i) != 'A') {
+                toLeft--;
+                continue;
+            }
+            if (name.charAt(i) == 'A') {
+                flag = false;
+                toLeft++;
+            } else {
+                break;
+            }
+        }
+        flag = false;
+
+        int toRight = 0;
+        for (int i = name.length() - 1; 0 <= i; i--) {
+            if (flag && name.charAt(i) != 'A') {
+                toRight--;
+                continue;
+            }
+            if (name.charAt(i) == 'A') {
+                toRight++;
+            } else {
+                break;
+            }
+        }
+
+        return counter - Math.max(toLeft, toRight);
+    }
 }
 
 public class SourceCode {
