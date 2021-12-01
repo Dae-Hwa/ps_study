a088c075d514ebadd5cc7c7c822631a42d855601
Dae-Hwa <daehwa_jeong@outlook.com>
2021-12-01 17:18:01 +0900

    백준 마방진(2045) 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
 
 class Main {
 
@@ -16,91 +19,52 @@ class Main {
             board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
         }
 
-        Set<List<Integer>> zeros = new HashSet<>();
-        int og = 0;
+        List<Node> zeros = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            int horizon = 0;
-            int vertical = 0;
-
-            boolean horizonFlag = false;
-            boolean verticalFlag = false;
-
             for (int j = 0; j < N; j++) {
-                horizon += board[i][j];
-                vertical += board[j][i];
-
                 if (board[i][j] == 0) {
-                    zeros.add(List.of(i, j));
-                    horizonFlag = true;
+                    zeros.add(new Node(i, j));
                 }
-                if (board[j][i] == 0) {
-                    verticalFlag = true;
-                    zeros.add(List.of(j, i));
-                }
-            }
-
-            if (!horizonFlag) {
-                og = Math.max(og, horizon);
-            }
-
-            if (!verticalFlag) {
-                og = Math.max(og, vertical);
             }
         }
 
-        if (og == 0) {
-            og = (Arrays.stream(board)
-                        .flatMapToInt(ints -> Arrays.stream(ints))
-                        .sum()) / 2;
+        Node[] verticals = new Node[]{
+                new Node(0, -1),
+                new Node(-1, 0),
+                new Node(0, 1),
+                new Node(1, 0)
+        };
+
+        Node[] diagonals = new Node[]{
+                new Node(-1, -1),
+                new Node(-1, 1),
+                new Node(1, -1),
+                new Node(1, 1)
+        };
+
+        int sum = sumOfLine(board, verticals, diagonals);
+
+        if (sum == 0) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sum += board[i][j];
+                }
+            }
+            sum /= 2;
         }
 
+        int i = 0;
         while (!zeros.isEmpty()) {
-            List<List<Integer>> toBeRemove = new ArrayList<>();
-            for (List<Integer> zero : zeros) {
-                int horizon = 0;
-                int vertical = 0;
+            i = i % zeros.size();
+            Node zero = zeros.get(i);
 
-                int horizonFlag = 0;
-                int verticalFlag = 0;
+            boolean removed = fillZero(board, zeros, verticals, sum, i, zero);
 
-                int h = zero.get(0);
-                int v = zero.get(1);
-
-                for (int i = h; i < h + N; i++) {
-                    int x = i % N;
-                    int y = v;
-
-                    horizon += board[x][y];
-
-                    if (board[x][y] == 0) {
-                        horizonFlag++;
-                    }
-                }
-
-                for (int i = h; i < h + N; i++) {
-                    int x = h;
-                    int y = i % N;
-
-                    vertical += board[x][y];
-
-                    if (board[x][y] == 0) {
-                        verticalFlag++;
-                    }
-                }
-
-                if (horizonFlag == 1) {
-                    board[h][v] = og - horizon;
-                } else if (verticalFlag == 1) {
-                    board[h][v] = og - vertical;
-                } else {
-                    continue;
-                }
-
-                toBeRemove.add(zero);
+            if (!removed && zero.canMoveToDiagonal()) {
+                fillZero(board, zeros, diagonals, sum, i, zero);
             }
-
-            zeros.removeAll(toBeRemove);
+            i++;
         }
 
         for (int[] r : board) {
@@ -112,6 +76,120 @@ class Main {
 
             System.out.println(sj);
         }
+    }
+
+    private static boolean fillZero(int[][] board, List<Node> zeros, Node[] directions, int sum, int i, Node zero) {
+        for (Node direction : directions) {
+
+            boolean hasZero = false;
+            int curSum = 0;
+            for (int j = 1; j <= N - 1; j++) {
+                int nextX = nextBy(zero.x, direction.x, j);
+                int nextY = nextBy(zero.y, direction.y, j);
+                int next = board[nextX][nextY];
+
+                if (next == 0) {
+                    hasZero = true;
+                }
+                curSum += next;
+            }
+
+            if (!hasZero && !zeros.isEmpty()) {
+                board[zero.x][zero.y] = sum - curSum;
+                zeros.remove(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static int sumOfLine(int[][] board, Node[] verticals, Node[] diagonals) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+
+                for (Node direction : verticals) {
+                    int curSum = sum(i, j, board, direction);
+                    if (curSum != 0) {
+                        return curSum;
+                    }
+                }
+
+                if (new Node(i, j).canMoveToDiagonal()) {
+                    for (Node direction : diagonals) {
+                        int curSum = sumD(i, j, board, direction);
+                        if (curSum != 0) {
+                            return curSum;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return 0;
+    }
+
+    private static int sum(int x, int y, int[][] board, Node direction) {
+        int sum = 0;
+        for (int k = 1; k <= N; k++) {
+            int nextX = nextBy(x, direction.x, k);
+            int nextY = nextBy(y, direction.y, k);
+            int next = board[nextX][nextY];
+            sum += next;
+
+            if (next == 0) {
+                return 0;
+            }
+        }
+
+        return sum;
+    }
+
+    private static int sumD(int x, int y, int[][] board, Node direction) {
+        int sum = 0;
+        for (int k = 0; k < N; k++) {
+            int nextX = x + direction.x * k;
+            int nextY = y + direction.y * k;
+
+            if (!(0 <= nextX && 0 <= nextY && nextX < 3 && nextY < 3)) {
+                return 0;
+            }
+
+            int next = board[nextX][nextY];
+            sum += next;
+
+            if (next == 0) {
+                return 0;
+            }
+        }
+
+        return sum;
+    }
+
+    private static int nextBy(int cur, int next, int cnt) {
+        return (cur + (next * cnt) + 3) % N;
+    }
+}
+
+class Node {
+    int x;
+    int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean canMoveToDiagonal() {
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+
+        if (absX == 1 && absY == 1) {
+            return true;
+        }
+
+        return absX != 1 && absY != 1;
     }
 }
 
