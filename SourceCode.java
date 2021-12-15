d690531507bd11d92bb3dca3a6f7b197895548e5
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2021-12-15 20:13:06 +0900

    구름 템플릿 추가

--- SourceCode.java
+++ SourceCode.java
@@ -1,3 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
+
 public class SourceCode {
     public static void main(String[] args) {
         Object[] inputs = new Object[]{
