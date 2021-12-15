d28d8c92fa8d58b42b23317fc3c0127b1e1602c7
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2021-12-15 20:23:09 +0900

    구름 점심은 햄버거 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,10 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] eatingTime = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+        int[] heatingTime = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+        Integer[] lookup = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            lookup[i] = i;
+        }
+
+        Arrays.sort(
+                lookup,
+                Comparator.comparingInt((Integer value) -> eatingTime[value])
+                          .reversed()
+        );
+
+        int time = 0;
+        int endTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            time += heatingTime[lookup[i]];
+            endTime--;
+            endTime = Math.max(endTime, eatingTime[lookup[i]]);
+        }
+
+        System.out.println(time + endTime);
     }
 }
 
