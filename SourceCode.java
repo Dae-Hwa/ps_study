472c8c670d07ef65e80a2c02347daa7941ff8acf
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2022-01-15 17:53:02 +0900

    백준 우유 축제 완료

--- SourceCode.java
+++ SourceCode.java
@@ -1,98 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
 
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        var N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         int[] stores = Arrays.stream(br.readLine().split(" "))
                              .mapToInt(Integer::valueOf)
                              .toArray();
 
-        Deque<Node> visits = new ArrayDeque<>();
-        var start = new Node(
-                stores,
-                -1,
-                2,
-                0
-        );
-        visits.push(start);
+        int max = 0;
+        int cur = 2;
 
-        var max = 0;
-        while (!visits.isEmpty()) {
-            var cur = visits.pop();
-            max = Math.max(cur.count, max);
-
-            var forNotBuying = cur.next();
-            if (forNotBuying.notBuy()) {
-                visits.push(forNotBuying);
-            }
-
-            var forBuying = cur.next();
-            if (forBuying.buy()) {
-                visits.push(forBuying);
+        for (var each : stores) {
+            if ((each == 0 && cur == 2) ||
+                (each == 1 && cur == 0) ||
+                (each == 2 && cur == 1)
+            ) {
+                cur = each;
+                max++;
             }
         }
 
         System.out.println(max);
-    }
-}
-
-class Node {
-    int[] stores;
-    int index;
-    int prev;
-    int count;
-
-    public Node(
-            int[] stores,
-            int index,
-            int prev,
-            int count
-    ) {
-        this.stores = stores;
-        this.index = index;
-        this.prev = prev;
-        this.count = count;
-    }
-
-    public Node next() {
-        return new Node(
-                stores,
-                index + 1,
-                prev,
-                count
-        );
-    }
-
-    public boolean buy() {
-        if (stores.length <= index) {
-            return false;
-        }
-
-        if (stores[index] - prev != 1 &&
-            stores[index] - prev != -2 &&
-            prev != -1
-        ) {
-            return false;
-        }
-
-        prev = stores[index];
-        count++;
-
-        return true;
-    }
-
-    public boolean notBuy() {
-        if (stores.length <= index) {
-            return false;
-        }
-
-        return true;
     }
 }
 
